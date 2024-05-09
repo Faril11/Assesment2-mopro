f2e66b419bb654fc1adf4e3618d7ec4e74b71fbf
Fazrul Ridha Alliandre <fazrulalliandre357@gmail.com>
2024-05-09 10:56:25 +0700

    initial commit

---
+++ app/src/main/java/org/d3if0006/assesment2_mopro/MainActivity.kt
@@ -0,0 +1,46 @@
+package org.d3if0006.assesment2_mopro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.d3if0006.assesment2_mopro.ui.theme.Assesment2moproTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Assesment2moproTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Assesment2moproTheme {
+        Greeting("Android")
+    }
+}
